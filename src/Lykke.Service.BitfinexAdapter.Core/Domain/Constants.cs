cf82c636680761ea8a7b98b39aaae422074b472b
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-03-23 17:28:13 +0200

    X-API-KEY header added to Swagger

--- src/Lykke.Service.BitfinexAdapter.Core/Domain/Constants.cs
+++ src/Lykke.Service.BitfinexAdapter.Core/Domain/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const string BitfinexExchangeName = "bitfinex";
-        public const string XApiKeyHeaderName = "X-ApiKey";
+        public const string XApiKeyHeaderName = "X-API-KEY";
         public static string AuthenticationError = "X-API-KEY was not set or the associated ApiKey and ApiSecret are missing from config.";
     }
 }
